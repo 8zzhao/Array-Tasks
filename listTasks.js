d3adbbe4b8dbf9b2f5a05488f720fadfd75dd0e3
Bruce <31480915+8zzhao@users.noreply.github.com>
2018-04-11 10:09:35 -0500

    Add files via upload

--- listTasks.js
+++ listTasks.js
@@ -12,24 +12,37 @@ function largest(list){
 //Task 2
 function reverse(list){
     let buddyFlat = [];
-    for (let a=0; a<list.length; a++0){
-        buddyFlat.push(list[a]);
+    for (let a=0; a<list.length; a++){
+        reverseList.push(buddyFlat[a]);
     }
 }
 
 //Task 3
 function cotains(list,e){
-
+    for (let a=0; a<list.length; a++){
+        if (list[a] == e){
+            return "True";
+        }
+        return "False";
+    }
 }
 
 //Task 4
 function oddElements(list){
-
+    let oddFlatHead = [];
+    for (let a=1; a<a.length; a=a+2){
+        oddFlatHead.push(list[a]);
+    }
+    return oddFlatHead;
 }
 
 //Task 5
 function total(list){
-
+    let FlatIsLife = 0;
+    for (let a=0; a<a.length; a++){
+        FlatIsLife = FlatIsLife + list[a];
+    }
+    return FlatIsLife;
 }
 
 //Task 6
